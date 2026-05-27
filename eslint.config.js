9cf78f777c943001203b0d6ff2cdd21fc00370b5
Brenno Luan <brennoluan8530@gmail.com>
2026-05-27 18:40:03 -0300

    Update Cypress configuration and enhance ESLint setup with Cypress plugin and Prettier integration

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,8 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import pluginCypress from 'eslint-plugin-cypress'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default defineConfig([
     globalIgnores(['dist']),
@@ -24,6 +26,15 @@ export default defineConfig([
         },
         rules: {
             'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+            ...eslintConfigPrettier.rules,
+        },
+    },
+    {
+        files: ['cypress/**/*.js'],
+        extends: [pluginCypress.configs.recommended],
+        rules: {
+            'cypress/no-unnecessary-waiting': 'off',
+            ...eslintConfigPrettier.rules,
         },
     },
 ])
